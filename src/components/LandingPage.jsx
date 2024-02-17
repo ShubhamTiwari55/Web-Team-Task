f396ee785fc1973ae072431b4d9de08f131f80c0
ShubhamTiwari55 <stofficial059@gmail.com>
2024-02-17 21:38:24 +0530

    added final changes

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,11 +8,12 @@ function LandingPage() {
 
       {/* Overlay */}
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-50 text-white">
-        <h1 className="text-4xl font-bold mb-8">Welcome to the Gallery</h1>
-        <div className="flex space-x-4">
-          <Link to="/collage" className="btn">Collage Gallery</Link>
-          <Link to="/staged-scrolling" className="btn">Staged Scrolling Gallery</Link>
-          <Link to="/carousel" className="btn">Carousel Gallery</Link>
+        <h1 className="text-5xl font-serif font-bold mb-8 text-red-600">Welcome to the Gallery!</h1>
+        <h1 className="text-2xl font-serif font-bold mb-8 text-green-500">Choose any of the style - </h1>
+        <div className="flex space-x-12 text-xl">
+          <Link to="/collage" className="btn  hover:text-yellow-300">Collage Gallery</Link>
+          <Link to="/staged-scrolling" className="btn  hover:text-yellow-300">Staged Scrolling Gallery</Link>
+          <Link to="/carousel" className="btn hover:text-yellow-300">Carousel Gallery</Link>
         </div>
       </div>
     </div>
